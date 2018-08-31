f926ae2661aad9f9bdc5163d4711d07789726a84
orok7 <orestmykytyn@gmail.com>
2018-08-31 10:23:49 +0300

    initial commit

---
+++ src/main/java/eins/angular/learning/bepartforangularlearning/BePartForAngularLearningApplication.java
@@ -0,0 +1,12 @@
+package eins.angular.learning.bepartforangularlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePartForAngularLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BePartForAngularLearningApplication.class, args);
+	}
+}
---
+++ src/test/java/eins/angular/learning/bepartforangularlearning/BePartForAngularLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package eins.angular.learning.bepartforangularlearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BePartForAngularLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
